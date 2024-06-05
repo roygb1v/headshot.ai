ddd0ebf299b0910a41c8e58070ee06880efc851e
Roy <cjhroy@outlook.com>
2024-06-06 03:32:49 +0800

    add timer to camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-import { useRef, useState, useLayoutEffect, useReducer } from "react";
-import { IconRotateClockwise, IconChevronLeft } from "@tabler/icons-react";
+import {
+  useRef,
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useReducer,
+} from "react";
+import {
+  IconRotateClockwise,
+  IconChevronLeft,
+  IconClockHour8,
+} from "@tabler/icons-react";
 import { MantineProvider, ActionIcon, Button } from "@mantine/core";
 
 import "@mantine/core/styles.css";
@@ -56,6 +66,10 @@ function VideoComponent() {
 
   const [src, setSrc] = useState("");
 
+  const [isTimer, setIsTimer] = useState(false);
+  const [clearIntervalId, setClearIntervalId] = useState(null);
+  const [timer, setTimer] = useState(3);
+
   const [options, dispatch] = useReducer(reducer, {
     constraints: {
       video: {
@@ -70,14 +84,35 @@ function VideoComponent() {
     dispatch({ type: "TOGGLE_FACING_MODE" });
   };
 
+  const handleCameraClick = () => {
+    const { videoWidth, videoHeight } = videoRef.current;
+
+    const context = canvasRef.current.getContext("2d");
+    context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
+
+    const data = canvasRef.current.toDataURL("image/png");
+
+    setSrc(data);
+
+    dispatch({ type: "NO_SHOW_IMAGE" });
+  };
+
+  useEffect(() => {
+    console.log("timer", timer);
+    if (timer < 0) {
+      clearInterval(clearIntervalId);
+      setTimer(3);
+    }
+  }, [timer, clearIntervalId]);
+
   useLayoutEffect(() => {
     const video = videoRef.current;
     video.setAttribute("autoplay", "");
     video.setAttribute("muted", "");
     video.setAttribute("playsinline", "");
-    console.log(window.innerHeight, window.innerWidth);
-    video.style.height = (window.innerHeight * 2) / 3 + "px";
-    video.style.width = (window.innerWidth * 2) / 3 + "px";
+    video.style.height = "478px";
+    video.style.width = "100%";
+    video.style.objectFit = "cover";
 
     navigator.mediaDevices
       .getUserMedia(options.constraints)
@@ -106,6 +141,24 @@ function VideoComponent() {
         justifyContent: "space-evenly",
       }}
     >
+      <ActionIcon
+        style={{
+          width: 40,
+          height: 40,
+          position: "absolute",
+          top: "4px",
+          right: 0,
+        }}
+        variant="transparent"
+        size="xl"
+        color={isTimer ? "#0ea5e9" : "#f5f5f4"}
+        aria-label="timer"
+        onClick={() => {
+          setIsTimer((prev) => !prev);
+        }}
+      >
+        <IconClockHour8 />
+      </ActionIcon>
       {options.shouldShow && (
         <ActionIcon
           variant="transparent"
@@ -141,7 +194,7 @@ function VideoComponent() {
         style={{
           display: "flex",
           justifyContent: "space-between",
-          width: (window.innerWidth * 2) / 3,
+          width: window.innerWidth,
           margin: "auto",
         }}
       >
@@ -162,22 +215,18 @@ function VideoComponent() {
             />
           ) : null}
         </ActionIcon>
-
         <Button
           disabled={options.loading}
-          onClick={async () => {
-            const { videoWidth, videoHeight } = videoRef.current;
-            console.log(videoWidth, videoHeight);
-
-            const context = canvasRef.current.getContext("2d");
-            context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
-
-            const data = canvasRef.current.toDataURL("image/png");
-            console.log(data);
-
-            setSrc(data);
-
-            dispatch({ type: "NO_SHOW_IMAGE" });
+          onClick={() => {
+            if (!isTimer) {
+              handleCameraClick();
+              return;
+            }
+            setTimeout(handleCameraClick, 3000);
+            const id = setInterval(() => {
+              setTimer((prev) => prev - 1);
+            }, 1000);
+            setClearIntervalId(id);
           }}
           style={{
             width: 80,
@@ -186,7 +235,10 @@ function VideoComponent() {
             borderRadius: "100%",
             margin: "auto",
           }}
-        />
+        >
+          {" "}
+          {isTimer && <h1 style={{ color: "gold", margin: 0 }}>{timer}</h1>}
+        </Button>
         <ActionIcon
           style={{ width: 80, height: 80 }}
           variant="outline"
